760088b6153a9444467f803be71bc8365da4a69d
leonpali <>
2025-03-20 12:55:09 +0100

    Initial Commit

---
+++ src/test/java/projects/java/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.java.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
