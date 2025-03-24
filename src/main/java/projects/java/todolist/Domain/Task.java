5b7722ad0bba7f80e3c13f7cb123ac651c96c891
leonpali <>
2025-03-24 18:54:37 +0100

    Getter & Setter für Task hinzugefügt

--- src/main/java/projects/java/todolist/Domain/Task.java
+++ src/main/java/projects/java/todolist/Domain/Task.java
@@ -24,4 +24,44 @@ public class Task {
         this.description = description;
         this.completionDate = completionDate;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskTime getTimeValues() {
+        return timeValues;
+    }
+
+    public CompletionDate getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTimeValues(TaskTime timeValues) {
+        this.timeValues = timeValues;
+    }
+
+    public void setCompletionDate(CompletionDate completionDate) {
+        this.completionDate = completionDate;
+    }
 }
