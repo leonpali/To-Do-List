407906a743cc0e08f09b56f7f289cebc91459701
leonpali <>
2025-03-26 15:10:42 +0100

    CSS und iFrame laden im Browser richtig

---
+++ src/main/java/projects/java/todolist/config/WebSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package projects.java.todolist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity chainBuilder) throws Exception {
+        chainBuilder.authorizeHttpRequests(
+                c -> c.requestMatchers("/overview", "/error", "/css/**", "/img/**", "/add-task.html").permitAll()
+                        .anyRequest().authenticated()
+        ).csrf(AbstractHttpConfigurer::disable)
+                .headers(
+                        h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+                );
+        return chainBuilder.build();
+    }
+}
