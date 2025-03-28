3cee87cc3e775fbdcea077061a8f0e7a581564e7
leonpali <>
2025-03-28 15:03:51 +0100

    Test klappt wieder richtig, POST nicht doppelt ausgeführt

--- src/test/java/projects/java/todolist/web/OverviewControllerTest.java
+++ src/test/java/projects/java/todolist/web/OverviewControllerTest.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import projects.java.todolist.config.WebSecurityConfiguration;
+
+import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Import(OverviewController.class)
+@Import({OverviewController.class, WebSecurityConfiguration.class})
 @WebMvcTest(OverviewController.class)
 public class OverviewControllerTest {
 
@@ -32,6 +35,7 @@ public class OverviewControllerTest {
         mockMvc.perform(post("/overview")
                         .param("taskName", "Code pushen")
                         .param("taskTime", "1h 30m")
+                        .param("taskDate", String.valueOf(LocalDate.now()))
                         .param("taskDescription", "Code auf Github pushen"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(view().name("overview"))
