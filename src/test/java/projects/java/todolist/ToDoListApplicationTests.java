fa70eecfb90fd1400d7a0d4ef76447401934c978
leonpali <>
2025-05-07 14:27:23 +0200

    Datenbank für Test Runtime hinzugefügt

--- src/test/java/projects/java/todolist/ToDoListApplicationTests.java
+++ src/test/java/projects/java/todolist/ToDoListApplicationTests.java
@@ -1,9 +1,11 @@
 package projects.java.todolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class ToDoListApplicationTests {
 
 	@Test
